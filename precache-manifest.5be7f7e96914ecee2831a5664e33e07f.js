6011dd6e0293d49e08861383e25bb78342c19e86
adityasshinde <s.adityashinde11@gmail.com>
2022-12-26 01:40:43 +0530

    Updates

---
+++ precache-manifest.5be7f7e96914ecee2831a5664e33e07f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad4838d84f1a37faa5d",
+    "url": "/adityasshinde/Order-Meals/static/css/main.59488313.chunk.css"
+  },
+  {
+    "revision": "cad4838d84f1a37faa5d",
+    "url": "/adityasshinde/Order-Meals/static/js/main.3c03b6da.chunk.js"
+  },
+  {
+    "revision": "aba556edc1be78c31fc3",
+    "url": "/adityasshinde/Order-Meals/static/js/runtime~main.27b7f754.js"
+  },
+  {
+    "revision": "fddd0e963fa6f3961d28",
+    "url": "/adityasshinde/Order-Meals/static/js/2.4672411b.chunk.js"
+  },
+  {
+    "revision": "4af07b562c0602e03473",
+    "url": "/adityasshinde/Order-Meals/static/js/3.a772fac8.chunk.js"
+  },
+  {
+    "revision": "c7e4a4f16125b755e06f396d600e59ce",
+    "url": "/adityasshinde/Order-Meals/static/media/entrancebg.c7e4a4f1.jpg"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/adityasshinde/Order-Meals/static/media/meals.55c74990.jpg"
+  },
+  {
+    "revision": "c5890d357777d8d368341fe0455216e2",
+    "url": "/adityasshinde/Order-Meals/index.html"
+  }
+];
