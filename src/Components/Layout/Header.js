f93f0d1af8b4bc513c4ac7caa96eabdb52b5f4de
adityasshinde <s.adityashinde11@gmail.com>
2022-12-27 00:56:15 +0530

    responsive property added

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,15 +2,19 @@ import React,{Fragment} from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import classes from './Header.module.css';
 import mealsimg from '../../assests/meals.jpg';
+import iconApp from '../../assests/foodordering.png';
 
 const Header=props=>{
      return <Fragment>
              <header className={classes.header}>
+           <div className={classes.title}>
+                <img src={iconApp} className={classes.icon}></img>
                 <h1>MealsAtDoor.com</h1>
+        </div>
                 <HeaderCartButton onClick={props.onShownCart} />
              </header>
              <div className={classes["main-image"]}>
-            <img src={mealsimg} alt='Image Loading'></img>
+            <img src={mealsimg} alt='Image Loading' className={classes.ima} />
              </div>
      </Fragment>
 };
